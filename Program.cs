79458de0178d6a9982d1ce9ab1b5a809893397d5
Ethan Mabry <ethanlmabry@gmail.com>
2021-07-25 21:41:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaringVariables
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+            double myDouble = 5.5;
+            string myString = "Hello";
+            bool myBool = false;
+            char myChar = 'e';
+
+            Console.WriteLine(myInt);
+        }
+    }
+}
